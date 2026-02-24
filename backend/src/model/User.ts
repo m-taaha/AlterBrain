c3a8baca8d601c0ef41899acffa749ff9227b606
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-02-25 00:35:49 +0530

    feat: add user model

---
+++ backend/src/model/User.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+}
+
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+   password: {
+      type: String,
+      required: [true, 'Please enter your password'],
+   }
+},
+{timestamps: true}
+)
+
+export const User = mongoose.model<IUser>("User", userSchema);
