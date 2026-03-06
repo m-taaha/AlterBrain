fa3387461defe0727ca3d38a8c2e53eb2b01d109
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-07 04:28:21 +0530

    feat: add create share brain link

---
+++ backend/src/controllers/brainController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import CryptoJS from "crypto-js";
+import { Link } from "../model/Link";
+import { Content } from "../model/Content";
+
+export const shareBrain = async (req: Request, res: Response) => {
+    try {
+
+        if(!req.user) {
+            return res.status(401).json({
+                message: "Unauthorized",
+            });
+        }
+
+        const {share} = req.body;
+
+        if(share) {
+
+            const existingLink = await Link.findOne({
+                userId: req.user._id,
+            });
+
+            if(existingLink) {
+                return res.json({
+                    shareLink: existingLink.hash,
+                });
+            }
+
+            const random = CryptoJS.lib.WordArray.random(16).toString();
+            const hash = CryptoJS.SHA256(random).toString(CryptoJS.enc.Hex);
+
+            const link = await Link.create({
+                hash,
+                userId: req.user._id,
+            });
+
+            return res.json({
+                shareLink: link.hash,
+            });
+
+        } else {
+
+            await Link.deleteOne({
+                userId: req.user._id,
+            });
+
+            return res.json({
+                message: "Sharing disabled",
+            })
+
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server Error", 
+        })
+    }
+}
